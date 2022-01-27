3234879c27976479a84cdec34d7fe8c9d5b44d44
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-01-27 17:11:20 +0200

    feat: add data from api except responders

--- src/components/Incidents/IncidentList/index.js
+++ src/components/Incidents/IncidentList/index.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import cx from "clsx";
+import dayjs from "dayjs";
 import { Icon } from "../../Icon";
 
 export function IncidentList({ list, ...rest }) {
@@ -21,47 +23,68 @@ export function IncidentList({ list, ...rest }) {
 }
 
 function IncidentItem({ incident, ...rest }) {
-  const { title, id, description, severity, status, type } = incident;
+  const { title, id, created_at: createdAt, severity, status } = incident;
+
+  const getAge = () => {
+    const now = dayjs();
+    let date;
+    const create = dayjs(createdAt);
+    if (now.diff(create, "day") <= 31) {
+      date = `${now.diff(create, "day")} days`;
+    } else if (now.diff(create, "month") <= 12) {
+      date = `${now.diff(create, "month")} month`;
+    } else if (now.diff(create, "month" > 12)) {
+      date = `${now.diff(create, "year")} year`;
+    }
+    return date;
+  };
   return (
     <Link to={`/incidents/${id}`} {...rest}>
       <div className="px-6 py-3 grid grid-cols-7 items-center">
         <div className="text-gray-900 col-span-2 text-sm leading-5 font-medium">
-          Incident name
+          {title}
         </div>
         <div className="flex flex-row items-center">
           <Icon name="caretDoubleUp" />
           <p className="text-gray-900 text-sm leading-5 font-normal ml-3">
-            High
+            {severity === 2 ? "High" : "Low"}
           </p>
         </div>
         <div>
           <button
-            className="bg-light-green text-gray-800 text-xs leading-4 font-medium py-0.5 px-2.5 rounded-10px"
+            className={cx(
+              "text-gray-800 text-xs leading-4 font-medium py-0.5 px-2.5 rounded-10px",
+              status === "Open" ? "bg-light-green" : "bg-gray-100"
+            )}
             type="button"
           >
-            Active
+            {status === "Open" ? "Active" : "Resolved"}
           </button>
         </div>
-        <div className="text-gray-400 text-sm">3 days</div>
+        <div className="text-gray-400 text-sm">{getAge()}</div>
         <div className="text-gray-400 text-sm col-span-2 flex flex-row">
-          <div className="flex flex-row mr-4 items-center">
-            <img
-              className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
-              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-              alt=""
-            />
-            <p className="ml-1 text-sm">John Ower</p>
-          </div>
-          <div className="flex flex-row  mr-4 items-center">
-            <img
-              className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
-              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-              alt=""
-            />
-            <p className="ml-1 text-sm">John Werker</p>
-          </div>
+          {[
+            {
+              image:
+                "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+              name: "John Ower"
+            },
+            {
+              image:
+                "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+              name: "John Werker"
+            }
+          ].map(({ image, name }) => (
+            <div className="flex flex-row mr-4 items-center" key={name}>
+              <img
+                className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
+                src={image}
+                alt=""
+              />
+              <p className="ml-1 text-sm">{name}</p>
+            </div>
+          ))}
         </div>
-        {/* <div className="text-gray-400 text-sm">type: {type}</div> */}
       </div>
     </Link>
   );
