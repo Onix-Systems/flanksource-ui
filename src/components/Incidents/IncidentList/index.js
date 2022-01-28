cb3a073cbbeb7d6d7afa2307bd14d24b6b0c3a88
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-01-28 10:50:13 +0200

    fix: fix console warnings and align header of table

--- src/components/Incidents/IncidentList/index.js
+++ src/components/Incidents/IncidentList/index.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import cx from "clsx";
 import dayjs from "dayjs";
 import { Icon } from "../../Icon";
@@ -12,30 +12,30 @@ export function IncidentList({ list, ...rest }) {
         {...rest}
       >
         <thead className="rounded-md sticky top-16 z-20">
-          <tr className="px-6 py-3 grid grid-cols-7 gap-0 border-b border-gray-300 uppercase bg-lightest-gray rounded-t-md">
+          <tr className="px-6 py-3 grid grid-cols-7 gap-0 border-b border-gray-300 uppercase bg-lightest-gray rounded-t-md  items-center">
             <th
-              className="text-gray-800 text-xs col-span-2 sticky top-0"
+              className="text-gray-800 text-xs col-span-2 sticky top-0 text-left"
               scope="row"
             >
               Name
             </th>
-            <th className="text-gray-400 text-xs sticky top-0">Severity</th>
-            <th className="text-gray-400 text-xs sticky top-0">Status</th>
-            <th className="text-gray-400 text-xs sticky top-0">Age</th>
-            <th className="text-gray-400 text-xs col-span-2 sticky top-0">
+            <th className="text-gray-400 text-xs sticky top-0 text-left">
+              Severity
+            </th>
+            <th className="text-gray-400 text-xs sticky top-0 text-left">
+              Status
+            </th>
+            <th className="text-gray-400 text-xs sticky top-0 text-left">
+              Age
+            </th>
+            <th className="text-gray-400 text-xs col-span-2 sticky top-0 text-left">
               Responders
             </th>
           </tr>
         </thead>
         <tbody className="flex-1 overflow-y-auto">
-          {list.map((incident, index) => (
-            <tr
-              data-row-key={index + 1}
-              key={incident.id}
-              className="last:border-b-0 border-b"
-            >
-              <IncidentItem incident={incident} />
-            </tr>
+          {list.map((incident) => (
+            <IncidentItem incident={incident} key={incident.id} />
           ))}
         </tbody>
       </table>
@@ -43,7 +43,7 @@ export function IncidentList({ list, ...rest }) {
   );
 }
 
-function IncidentItem({ incident, ...rest }) {
+function IncidentItem({ incident }) {
   const { title, id, created_at: createdAt, severity, status } = incident;
 
   const getAge = () => {
@@ -59,54 +59,59 @@ function IncidentItem({ incident, ...rest }) {
     }
     return date;
   };
+  const navigate = useNavigate();
+  const goToIncidentsDetailById = (id) => {
+    navigate(`/incidents/${id}`);
+  };
   return (
-    <Link to={`/incidents/${id}`} {...rest}>
-      <tr className="px-6 py-3 grid grid-cols-7 items-center">
-        <td className="text-gray-900 col-span-2 text-sm leading-5 font-medium">
-          {title}
-        </td>
-        <td className="flex flex-row items-center">
-          <Icon name="caretDoubleUp" />
-          <p className="text-gray-900 text-sm leading-5 font-normal ml-3">
-            {severity === 2 ? "High" : "Low"}
-          </p>
-        </td>
-        <td>
-          <button
-            className={cx(
-              "text-gray-800 text-xs leading-4 font-medium py-0.5 px-2.5 rounded-10px",
-              status === "open" ? "bg-light-green" : "bg-gray-100"
-            )}
-            type="button"
-          >
-            {status === "open" ? "Active" : "Resolved"}
-          </button>
-        </td>
-        <td className="text-gray-400 text-sm">{getAge()}</td>
-        <td className="text-gray-400 text-sm col-span-2 flex flex-row">
-          {[
-            {
-              image:
-                "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-              name: "John Ower"
-            },
-            {
-              image:
-                "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-              name: "John Werker"
-            }
-          ].map(({ image, name }) => (
-            <div className="flex flex-row mr-4 items-center" key={name}>
-              <img
-                className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
-                src={image}
-                alt=""
-              />
-              <p className="ml-1 text-sm">{name}</p>
-            </div>
-          ))}
-        </td>
-      </tr>
-    </Link>
+    <tr
+      className="px-6 py-3 grid grid-cols-7 items-center last:border-b-0 border-b cursor-pointer"
+      onClick={() => goToIncidentsDetailById(id)}
+    >
+      <td className="text-gray-900 col-span-2 text-sm leading-5 font-medium">
+        {title}
+      </td>
+      <td className="flex flex-row items-center">
+        <Icon name="caretDoubleUp" />
+        <p className="text-gray-900 text-sm leading-5 font-normal ml-3">
+          {severity === 2 ? "High" : "Low"}
+        </p>
+      </td>
+      <td>
+        <button
+          className={cx(
+            "text-gray-800 text-xs leading-4 font-medium py-0.5 px-2.5 rounded-10px",
+            status === "open" ? "bg-light-green" : "bg-gray-100"
+          )}
+          type="button"
+        >
+          {status === "open" ? "Active" : "Resolved"}
+        </button>
+      </td>
+      <td className="text-gray-400 text-sm">{getAge()}</td>
+      <td className="text-gray-400 text-sm col-span-2 flex flex-row">
+        {[
+          {
+            image:
+              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+            name: "John Ower"
+          },
+          {
+            image:
+              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+            name: "John Werker"
+          }
+        ].map(({ image, name }) => (
+          <div className="flex flex-row mr-4 items-center" key={name}>
+            <img
+              className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
+              src={image}
+              alt=""
+            />
+            <p className="ml-1 text-sm">{name}</p>
+          </div>
+        ))}
+      </td>
+    </tr>
   );
 }
